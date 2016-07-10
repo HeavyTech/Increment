e6a5b196264299e73eca4cb89fdf30febf4f5066
Jose Virgen <j.virgen87@gmail.com>
2016-07-10 16:12:52 -0700

    Increment App

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var count2 = 50
+    
+    @IBOutlet weak var label:UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+     
+           }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func incrementCount(){
+    
+    self.count++
+    self.label.text = "\(self.count)"
+    
+    }
+    
+    @IBAction func decrementCount(){
+        self.count2--
+        self.secondLabel.text = "\(self.count2)"
+        print(count2)
+        
     }
 
 
